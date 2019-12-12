fbfc7223f4ed036cb54223aa7c85553c1cd0dcb1
k4gi <k4gi@pm.me>
2019-12-12 23:06:42 +1100

    town sim huh

--- tappy.py
+++ tappy.py
@@ -117,6 +117,78 @@ def p_random(window, user_input_list):
                     window.addstr("Correct! You got it in " + str(tries) + " tries!\n")
                     user_input = "exit"
 
+def p_explore(window, user_input_list):
+    window.addstr("You're lost on an island. Can you find your way back home?\n-----\n")
+    window.addstr("You wake up. You try to breathe, and damp sand gets sucked into your nostrils. When you're done sputtering, you notice the gentle, rhythmic sound of waves and the sunlight warm on your skin. Gradually, you stand up. You're on a long, white beach. Behind you the ocean water stretches to the horizon. Before you lies a wild jungle. Welcome to The Island.\n")
+    user_prompt = ">> "
+    #input loop
+    user_input = ""
+    while user_input != "exit":
+        window.addstr(user_prompt)
+        user_input = get_string(window)
+        user_input_list2 = user_input.split()
+        if user_input_list2:
+            window.addstr(user_input_list2[0] + "\n")
+
+def p_town(window, user_invoke_list):
+    window.addstr("It's town management time, binch.\n")
+    user_prompt = ">> "
+
+    #declare town stats
+    town_day = 1
+    town_pop = 5
+    town_pop_starving = 0
+    town_pop_dead = 0
+    town_graveyard = 0
+    town_food = 20
+    town_wood = 20
+    town_houses = 1
+    town_house_progess = 0
+    town_art = 0
+    town_art_progress = 0
+    #jobs
+    job_forager = 0
+    job_cutter = 0
+    job_builder = 0
+    job_artisan = 0
+
+    #game loop
+    user_input = ""
+    while user_input != "exit":
+        #status update
+        window.addstr("The sun rises on day " + town_day + " of The Town.\n")
+        while town_food < town_pop: #feed the well fed
+            town_pop -= 1
+            town_pop_starving += 1
+        town_food -= town_pop
+        while town_food < town_pop_starving: #feed the starving
+            town_pop_starving -= 1
+            town_pop_dead += 1
+        town_food -= town_pop_starving
+        while town_food > 0 && town_pop_starving > 0: # satiate the starving
+            town_food -= 1
+            town_pop_starving -= 1
+            town_pop += 1
+        if town_houses > (town_pop + town_pop_starving)/5: #check for sprout
+            if random.randint(0,4) == 0:
+                town_pop += 1
+                window.addstr("A new townsperson has sprouted!\n")
+        window.addstr(town_pop + " people are well fed.\n" + town_pop_starving + " people are starving.\n" + town_pop_dead + " people have died.\n")
+        if town_house_progress > 0: #check for building progress
+            if town_house_progress > job_builder:
+                town_house_progress -= job_builder
+            else:
+                town_house_progress = 0
+                town_houses += 1
+                window.addstr("A new house has finished construction!\n")
+        window.addstr("The Town has " town_houses + " houses.\n")
+        window.addstr("The stockpiles hold " + town_food + " food,\n")
+        window.addstr("                and " + town_wood + " wood.\n")
+        window.addstr("What will the next day bring?\n\n")
+
+        window.addstr(user_prompt)
+        user_input = get_string(window)
+        user_input_list = user_input.split()
 
 def read_command(window, user_input_string):
     user_input_list = user_input_string.split()
@@ -133,6 +205,10 @@ def read_command(window, user_input_string):
             p_read(window, user_input_list)
         elif user_input_list[0] == "random":
             p_random(window, user_input_list)
+        elif user_input_list[0] == "explore":
+            p_explore(window, user_input_list)
+        elif user_input_list[0] == "town":
+            p_town(window, user_input_list)
         else:
             out_str = ""
             out_str += "Error: did not recognise command "
